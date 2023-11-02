db3a416ef5868a594c135add3f9264de2c099f10
himalmevada <mevadahimal2002@gmail.com>
2023-11-02 12:42:22 +0530

    Added files

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template,request
 
 application = Flask(__name__)
 app = application
@@ -13,10 +13,32 @@ ridge_model = pickle.load(open("./models/ridge.pkl", "rb"))
 standard_scaler = pickle.load(open("./models/scaler.pkl", "rb"))
 
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+@app.route("/predictdata",methods=["GET","POST"])
+def predict_datapoint():
+    if request.method == "POST":
+        Temperature = float(request.form.get("Temperature"))
+        RH = float(request.form.get("RH"))
+        Ws = float(request.form.get("Ws"))
+        Rain = float(request.form.get("Rain"))
+        FFMC = float(request.form.get("FFMC"))
+        DMC = float(request.form.get("DMC"))
+        ISI = float(request.form.get("ISI"))
+        Classes = float(request.form.get("Classes"))
+        Region = float(request.form.get("Region"))
+
+        new_datapoint = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result = ridge_model.predict(new_datapoint)
+
+        return render_template("home.html",results=result[0])
+
+    else:
+        return render_template("home.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
